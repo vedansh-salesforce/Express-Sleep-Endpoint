872ee9eec838895cc0715b58902ec7de74b8319d
vedansh-salesforce <152013454+vedansh-salesforce@users.noreply.github.com>
2025-05-28 19:06:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-    let ms = req.query.ms;
-    setTimeout(() => {
-        res.sendStatus(200);
-    }, ms);
-})
+    try{
+        let ms = parseInt(req.query.ms) - 300;
+        ms = (ms < 0)? 0 : ms;
+        setTimeout(() => {
+            res.sendStatus(200);
+        }, ms);
+    } catch (e) {
+        res.sendStatus(400);
+    }
+});
+
+app.get('/persist', (req, res) => {
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App started. Port: ${port}`);
+  setInterval(() => {
+    fetch('https://express-sleep-endpoint.onrender.com/persist');
+  }, 60000);
+});
