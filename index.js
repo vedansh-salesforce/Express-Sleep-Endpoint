7d1fe4c441b092b3ad2b86bc1d08437bbe37da44
vedansh-salesforce <152013454+vedansh-salesforce@users.noreply.github.com>
2025-05-29 12:26:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.get('/', (req, res) => {
     console.log('Sleep called. Duration: ' + req.query.ms + ' ms.');
     try{
-        let ms = parseInt(req.query.ms) - 100;
+        let ms = parseInt(req.query.ms) - (parseInt(req.query.offset) || 0);
         ms = (ms < 0)? 0 : ms;
         setTimeout(() => {
             res.sendStatus(200);
